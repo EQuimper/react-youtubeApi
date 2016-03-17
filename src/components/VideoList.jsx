da7b4e1d33186fbc29c7df8484231d444b53055f
UrNightmares <quimperemanuel@gmail.com>
2016-03-17 15:12:32 -0600

    api on

---
+++ src/components/VideoList.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import VideoListItem from './VideoListItem.jsx';
+
+const VideoList = (props) => {
+  const videoItems = props.videos.map((video) => {
+    return <VideoListItem key={video.etag} video={video} />
+  });
+
+  return (
+      <div>
+        <ul className="col-md-4 list-group">
+          {videoItems}
+        </ul>
+      </div>
+  );
+};
+
+export default VideoList;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/SearchBar.jsx'
+import SearchBar from './components/SearchBar.jsx';
+import VideoList from './components/VideoList.jsx';
 const API_KEY = 'AIzaSyDW0PS3mvaWvAvcIHWGwFzDuyd7MudMVFU';
 
 
@@ -20,6 +21,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
